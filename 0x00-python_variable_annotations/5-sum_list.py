26920d3d5aba0ba52201fce3d757783f57da0a08
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-14 12:33:57 +0000

    [feat]: added documentation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from typing import List
 """Defines a function sum_list that takes one argument"""
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
