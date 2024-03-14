c435cac1b2a1f26caf970900e82a5d101458ff10
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-14 12:04:54 +0000

    [feat]: type-annotated function to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a function to_str that takes one argument, n, of type float"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
