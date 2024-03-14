9b7c338268dfb220428324e3f034beeb3b165bd8
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-14 12:32:27 +0000

    [feat]: complext types = list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""Defines a function sum_list that takes one argument"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Calculates the sum of elements in a list of floats"""
+    return sum(input_list)
