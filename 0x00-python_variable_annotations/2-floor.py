5032894549d667cb4fa01f434078eb2fc99c8fe0
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-14 12:01:30 +0000

    [feat]: type-annotated function floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines a function floor that takese one argument, n, of type float."""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float."""
+
+    # Implement the logic to calculate floor without using math.floor
+    return int(n)
