b6164bbc8ccbf7639e15d49ca7e1452d20d04826
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-14 11:57:38 +0000

    [feat]: basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Defines a function concat that takes two arguments,
+str1 and str2, both of type str
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the combined string."""
+    return str1 + str2
