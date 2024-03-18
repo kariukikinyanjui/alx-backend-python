7bd038f591d0319052f0a06d5265c18d536672b1
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-18 19:37:34 +0000

    [feat]: removed unnecessary module

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,11 +5,10 @@ import wait_n from 1-concurrent_coroutines
 """
 import asyncio
 import time
-from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> List[float]:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measure the total execution time of wait_n(n, max_delay) and
     returns the average time per call (total_time / n).
