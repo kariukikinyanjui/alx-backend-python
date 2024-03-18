1be433857f49e69d933ac53c1d54ef3e64ed5b7f
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-18 19:47:12 +0000

    [feat]: created a scheduled asynchronous task without actually running it within the function itself

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Create and manage coroutines"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create and returns a Task object that will execute the
+    wait_random coroutine
+
+    Args:
+        max_delay: The maximum delay for the wait_random coroutine
+
+    Returns:
+        An asyncio.Task object representing the execution
+        of the wait_random
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
