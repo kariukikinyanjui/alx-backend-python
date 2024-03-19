6f0bb8d4f15cf55abf8f4245006168ea769797f5
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-03-19 18:30:53 +0000

    [feat]: asynchronous generator with 1 sec delay

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,13 +5,14 @@ for generating random numbers
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random numbers
     between 0 and 10
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
