12e15d26c43459bbf74005d3e829da4bc0b75c5d
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-04-08 22:49:13 +0000

    [feat]: test tht KeyError is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found in nested map"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map['a']"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, error_mess):
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), error_mess)
+
 
 if __name__ == '__main__':
     unittest.main()
