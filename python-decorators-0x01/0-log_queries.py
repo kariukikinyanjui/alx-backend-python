5b4e76fb22889a23be367bc8d03fab301ce5d31b
kariukinyanjui <michaelkariuki7@gmail.com>
2024-12-04 19:17:53 +0000

    [feat]: included the datetime import for timestamping the log entries

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sqlite3
 import functools
+from datetime import datetime
 
 
 # Decorator to log SQL queries
@@ -8,7 +9,8 @@ def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = args[0]
-        print(f'Executing query: {query}')
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f'[{timestamp}] Executing query: {query}')
         return func(*args, **kwargs)
     return wrapper
 
