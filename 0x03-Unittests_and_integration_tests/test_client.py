44c5c2df60fe4e79e9e0bd9c35308aaa7b145270
kariukinyanjui <michaelkariuki7@gmail.com>
2024-12-14 07:28:05 +0000

    [feat]: imported get_json from utils

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,6 +3,7 @@ import unittest
 from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
+from utils import get_json
 
 
 class TestGithubOrgClient(unittest.TestCase):
