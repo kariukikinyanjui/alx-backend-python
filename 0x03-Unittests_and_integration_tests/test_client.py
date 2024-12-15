8ca11c010e8dd4509f70082c7593e99efeea28b1
kariukinyanjui <michaelkariuki7@gmail.com>
2024-12-15 20:00:46 +0000

    [feat]: parameterize tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -85,6 +85,24 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                     "https://api.github.com/orgs/test_org/repos")
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        '''
+        Test that GithubOrgClient.has_license returns the
+        correct boolean value.
+
+        Args:
+            repo(dict): The repository information.
+            license_key(str): The license key to check for.
+            expected(bool): The expected return value.
+        '''
+        client = GithubOrgClient("test_org")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
