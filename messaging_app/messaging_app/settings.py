a5c913a360617adf63fd7a4bb9272886cd2bf73a
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-12-18 10:22:32 +0300

    [feat]: configured to enforce authentication globally

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': 'rest_framework.permissions.IsAuthenticated',
+    'DEFAULT_AUTHENTICATION_CLASSES': 'rest_framework.authentication.SessionAuthentication',
+
 }
 
 
