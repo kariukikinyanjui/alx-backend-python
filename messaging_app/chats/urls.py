88fe604ad993a65c03b76a4ec9a464f14be39364
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-12-18 23:37:32 +0300

    [feat]: added option for non-nested message management

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -10,5 +10,4 @@ router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('conversations/<str:conversation_pk>/messages/', message_list, name='conversation-messages'),
 ]
