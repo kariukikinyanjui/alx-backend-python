e375d227dfa48f0d1eabced421fc7753b381a5e3
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-12-18 13:29:45 +0300

    [feat]: modified to handle nested relationships properly

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -5,25 +5,40 @@ from .models import Conversation
 
 
 class UserSerializers(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True)
+
+
     class Meta:
         model = User
         field = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'created_at']
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)
+    sender = serializers.SerializerMethodField()
 
 
     class Meta:
         model = Message
         fields = ['message_id', 'sender', 'message_body', 'sent_at']
 
+    def get_sender(self, obj):
+        return f"{obj.sender.first_name} {obj.sender.last_name}"
+
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    participant_count = serializers.SerializerMethodField()
 
 
     class Meta:
         model = Conversation
         field = ['conversation_id', 'participants', 'messages', 'created_at']
+
+    def get_participant_count(self, obj):
+        return obj.participant.count()
+
+    def validate(self, data):
+        if 'participants' in data and len(data['participants']) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return data
