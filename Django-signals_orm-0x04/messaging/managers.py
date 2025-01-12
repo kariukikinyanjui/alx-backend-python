f890b54d889de1de4ba3214719fa69f2e03700a3
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-01-12 15:46:29 +0300

    [feat]: custom mananger for filtering unread messages

---
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class UnreadMessageManager(models.Manager):
+    '''
+    custom manager to filter unread messages for a specific user.
+    '''
+    def for_user(self, user):
+        '''
+        Returns unread messages for the given user.
+        '''
+        return self.filter(receiver=user, read=False).only("id", "content", "timestamp", "sender")
