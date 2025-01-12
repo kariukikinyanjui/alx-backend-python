96c50759ed548365bbc8b015b7ff8db4033bff8e
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-01-12 11:32:32 +0300

    [feat]: added a parent_message field as a self-referential foreign key to represent replies

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -10,9 +10,10 @@ class Message(models.Model):
     edited = models.BooleanField(default=False)
     edited_at = models.DateTimeField(null=True, blank=True)
     edited_by = models.ForeignKey(User, null=True, blank=True, related_name="edited_messages", on_delete=models.SET_NULL)
+    parent_message = models.ForeignKey("self", null=True, blank=True, related_name="replies", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Message from {self.sender} to {self.receiver}"
+        return f"Message from {self.sender} to {self.receiver} - {self.content[:20]}"
 
 
 class MessageHistory(models.Model):
