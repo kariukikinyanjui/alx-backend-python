2887de75d69e57e4e04bd2722b152af79df4a19b
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-01-12 10:59:47 +0300

    [feat]: ensure the signal is registered when the app is ready

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -8,3 +8,11 @@ class MessagingConfig(AppConfig):
 
     def ready(self):
         import messaging.signals
+
+
+class DjangoChatConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "DjangoChat"
+
+    def ready(self):
+        import DjangoChat.signals # Register the signals
