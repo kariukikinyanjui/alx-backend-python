f5f1c7a4a6975f32a2385805cdc494ad4630a650
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-01-24 20:04:14 +0300

    [feat]: configured settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'chats',
+    'django_filters',
 ]
 
 AUTH_USER_MODEL = 'chats.User'
@@ -39,7 +40,11 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
 }
 
 SIMPLE_JWT = {
