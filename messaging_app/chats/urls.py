1892cacd9cabcd14b29e9d2eddb330f8204c5de3
kariukikinyanjui <michaelkariuki7@gmail.com>
2025-01-24 16:48:16 +0300

    [feat]: RESTful nested routing while adding authentication capabiliites

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,18 +2,20 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 from rest_framework import routers
-
+from rest_framework_nested.routers import NestedDefaultRouter
 
 # Initialize the router and register the viewsets
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
 
+# Create nested router for messages
+messages_router = NestedDefaultRouter(
+    router, r'conversations', lookup='conversation'
+)
+messages_router.register(r'messages', MessageViewSet, basename='message')
+
 urlpatterns = [
     path('', include(router.urls)),
-    path(
-        'conversations/<uuid:conversation_id>/messages/',
-        MessageViewSet.as_view({'get': 'list', 'post': 'create'}),
-        name='conversation-messages'
-    ),
+    path('', include(messages_router.urls)),
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
